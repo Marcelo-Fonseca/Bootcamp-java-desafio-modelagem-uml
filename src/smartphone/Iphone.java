82f7ec0df97fa52c530ca846e0d4a8e8547b1b8c
Marcelo-Fonseca <projetosweb11@gmail.com>
2024-10-19 21:58:12 -0300

    Atualização de video e arquivo iphone.java

--- src/smartphone/Iphone.java
+++ src/smartphone/Iphone.java
@@ -12,13 +12,26 @@ public class Iphone {
         NavegadorDeInternet internet = iphone7;
         ReprodutorMusical ipod = iphone7;
 
+        System.out.println("------*Desafio de UML e Java*-------------------");
+        System.out.println("------IPhone7-------------------");
         //usando a intert----
+        System.out.println("usando internet");
         iphone7.abrirNavegador();
+        iphone7.adicionarNovaAba();
+        iphone7.abrirYahoo();
         iphone7.enviarEmail();
+        iphone7.exibirPagina();
+        iphone7.atualizarPagina();
+        System.out.println("-------------------------");
         //usando telefone
+        System.out.println("usando telefone");
         iphone7.atenderLigacao();
+        iphone7.desligarLigacao();
         iphone7.iniciarCorreioDeVoz();
+        iphone7.enviarSms();
+        System.out.println("-------------------------");
         //usando ipod
+        System.out.println("usando ipod");
         iphone7.tocarMusica();
         iphone7.trocarMusica();
         iphone7.selecionarMusica();
